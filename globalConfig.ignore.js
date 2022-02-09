ea23b9bae3bfb05c2cdc407a48ee05e5d5e12f3e
wwang33 <wwang33@ncsu.edu>
2022-02-09 09:14:42 -0800

    try to remove ignore

--- globalConfig.ignore.js
+++ globalConfig.ignore.js
@@ -1,7 +1,15 @@
+// const globalConfigIgnore = {
+//      simplifiedInterfaceFor110: false,
+//      axiosExpressBaseURL: "http://localhost:9090/",
+//      axiosSpringBaseURL: "http://localhost:8082/",
+// }
+
+//below is for the remote server
+
 const globalConfigIgnore = {
      simplifiedInterfaceFor110: false,
-     axiosExpressBaseURL: "http://localhost:9090/",
-     axiosSpringBaseURL: "http://localhost:8082/",
+     axiosExpressBaseURL: "https://isnap.csc.ncsu.edu/node/",
+     axiosSpringBaseURL: "https://isnap.csc.ncsu.edu/servlet/service/",
 }
 
 export default globalConfigIgnore;
